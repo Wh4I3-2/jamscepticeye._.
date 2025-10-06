87a1a69325827bf962f839323ed0bc4838aeb2e4
Wh4I3-2 <154924333+Wh4I3-2@users.noreply.github.com>
2025-10-06 16:29:23 +0200

    menu fps fix

--- scripts/ui/menu/title_button.gd
+++ scripts/ui/menu/title_button.gd
@@ -5,9 +5,6 @@ extends Button
 @export var hover_margin: float = 4.0
 @export var focus_margin: float = 5.0
 
-@export var hover_slant: float = 0.075
-@export var focus_slant: float = 0.125
-
 @export var pointer: Control
 @export var focus_panel: Panel
 
@@ -22,7 +19,6 @@ var hover:       StyleBox
 var focus:       StyleBoxFlat
 var pressed_box: StyleBox
 
-var base_slant: float
 var base_width: float
 var base_x:     float
 
@@ -41,13 +37,6 @@ func _ready() -> void:
 	hover_base_margin   = hover.content_margin_left
 	pressed_base_margin = pressed_box.content_margin_left
 
-	var font: Font = get_theme_font("font")
-	if font is FontVariation: font_var = font.duplicate()
-
-	if font_var != null:
-		add_theme_font_override("font", font_var)
-		base_slant = font_var.variation_transform.x.y
-
 	if pointer != null:
 		base_width = pointer.size.x
 		base_x = pointer.position.x
@@ -64,17 +53,14 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	var margin: float = 0.0
-	var slant:  float = 0.0
 
 	var target_size: float = 0.0
 
 	if is_hovered(): 
 		margin = hover_margin
-		slant  = hover_slant
 		target_size = base_width
 	if has_focus(): 
 		margin = focus_margin
-		slant  = focus_slant
 		target_size = base_width
 	
 	focus.border_width_left = floori(lerpf(focus.border_width_left, focus_panel.size.x if has_focus() else 0.0, delta * speed))
@@ -83,9 +69,6 @@ func _process(delta: float) -> void:
 	hover.content_margin_left       = lerpf(hover.content_margin_left,       margin + hover_base_margin,   delta * speed)
 	pressed_box.content_margin_left = lerpf(pressed_box.content_margin_left, margin + pressed_base_margin, delta * speed)
 
-	if font_var != null:
-		font_var.variation_transform.x.y = lerpf(font_var.variation_transform.x.y, base_slant + slant, delta * speed)
-	
 	if pointer != null:
 		pointer.size.x     = lerpf(pointer.size.x,     target_size, delta * speed)
 		pointer.position.x = lerpf(pointer.position.x, base_x + base_width - target_size, delta * speed)
